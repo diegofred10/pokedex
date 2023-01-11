abb43b2ebc0eac066e60c3b8c546d17da083f3ab
Bryan <bryanpjpgi2b@hotmail.com>
2023-01-11 14:36:18 +0100

    changes on styles

--- Pokedex/src/views/Pokedex.vue
+++ Pokedex/src/views/Pokedex.vue
@@ -4,12 +4,8 @@
 <template>
   <header> 
     <h1>Pokedex F5</h1>
-    <img src="../assets/img/ash-logo.png" alt="logo-ash">
-  
-  
-  
+    <img src="../assets/img/769px-Pokebola-pokeball-png-0.png" alt="logo-ash">
   </header>
-
 
   <main>
     
