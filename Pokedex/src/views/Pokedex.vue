53a38fbdc399953b5703346f9b2e8c6cbc1d03c2
diegofred10 <diegoa.gsm@gmail.com>
2023-01-11 12:48:17 +0100

    estructure

---
+++ Pokedex/src/views/Pokedex.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <header> <h1>bryan</h1></header>
+
+
+  <main>
+    
+  </main>
+  <footer></footer>
+
+
+
+</template>
+
+
+<style scooped>
+@import "../assets/styles/styles.css";
+</style>
