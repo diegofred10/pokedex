6c897f5084044cb11b09e85e79e5c478d903a589
diegofred10 <diegoa.gsm@gmail.com>
2023-01-13 14:12:24 +0100

    card&nav

--- Pokedex/src/components/CardComponent.vue
+++ Pokedex/src/components/CardComponent.vue
@@ -1,7 +1,83 @@
 <script setup>
 </script>
 <template>
-
+<li>
+    <figure>
+      <img src="../assets/img/charizard.png" alt="name">
+    </figure>
+    <div class="contentPokemon">
+      <p><strong>v-bind: </strong></p>
+      <h3>Fuego</h3>
+      <p><strong>#6</strong></p>
+    </div>
+  </li>
 </template>
+
+
 <style scoped>
+li {
+  min-width: 50px;
+  border: 10px solid #ff3401;
+  border-radius: 20px;
+  overflow: hidden;
+}
+
+figure {
+  width: 75%;
+  position: relative;
+}
+
+img {
+  width: 100%;
+}
+
+h3 {
+  font-size: 16px;
+  color: #ffffff;
+  text-transform: uppercase;
+ 
+}
+
+h2 {
+  font-size: 16px;
+  color: #000000;
+  text-transform: uppercase;
+  margin-bottom: 4px;
+  margin-top: -24px;
+  margin-right: 25px;
+}
+
+
+
+
+
+p {
+  color: black;
+  font-family: "Roboto";
+  font-size: larger;
+}
+
+
+
+
+
+.contentPokemon {
+  position: relative;
+  padding: 20px;
+  background-color: #ff3401;
+
+}
+
+
+
+
+
+
+@media (max-width:500px) {
+  li {
+    border: none;
+    border-radius: 0;
+    margin-bottom: 50px;
+  }
+}
 </style>
